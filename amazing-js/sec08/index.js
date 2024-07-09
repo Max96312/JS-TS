abcbfcce03dcbb6f79d1bfbe6647270879ebe1a6
Max96312 <k0mp4ny16@gmail.com>
2024-07-10 01:26:39 +0900

    sec08: 모듈화 구문

---
+++ amazing-js/sec08/index.js
@@ -0,0 +1,10 @@
+// 일반 import & export 문법
+import { pi, sum } from "./math.js";
+// default export 문법
+import message from "./app.js";
+import lodashLibrary from "lodash";
+
+console.log(pi);
+console.log(sum(3, 4));
+console.log(message);
+console.log(lodashLibrary);
